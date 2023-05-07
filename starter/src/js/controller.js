cc6ad6d09c4484cc43f0db97675cef45cab7e8de
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-05-07 19:36:53 +0200

    showRecipe async function to get a single recipe

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -11,3 +11,48 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+// fetch request for a SINGLE recipe with error handling
+const showRecipe = async function () {
+  try {
+    const response = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc8f7'
+    );
+
+    const data = await response.json();
+
+    if (!response.ok)
+      throw new Error(`Recipe not found: ${data.message} (${response.status})`);
+
+    // My way
+    const recipe = ({
+      id,
+      tittle,
+      publisher,
+      source_url: sourceUrl,
+      image_url: image,
+      servings,
+      cooking_time: cookingTime,
+      ingredients,
+    } = data.data);
+
+    // Jonas way
+    // let { recipe } = data.data;
+    // recipe = {
+    //   id: recipe.id,
+    //   tittle: recipe.tittle,
+    //   publisher: recipe.publisher,
+    //   sourceUrl: recipe.source_url,
+    //   image: recipe.image_url,
+    //   servings: recipe.servings,
+    //   cookingTime: recipe.cooking_time,
+    //   ingredients: recipe.ingredients,
+    // };
+
+    console.log(recipe);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+showRecipe();
